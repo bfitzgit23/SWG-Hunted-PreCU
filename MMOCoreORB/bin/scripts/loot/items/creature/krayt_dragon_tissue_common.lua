6263f90e5de461932152927d1a0a40f40d567bc7
ben <dfitz@me.com>
2025-02-27 16:23:27 -0500

    Update krayt_dragon_tissue_common.lua

--- MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_common.lua
+++ MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_common.lua
@@ -6,9 +6,9 @@ krayt_dragon_tissue_common = {
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/component/weapon/blaster_power_handler_enhancement_krayt.iff",
 	craftingValues = {
-		{"mindamage",60,100,0},
-		{"maxdamage",100,255,0},
-		{"attackspeed",-1,-0.8,0},
+		{"mindamage",10,25,0},
+		{"maxdamage",28,58,0},
+		{"attackspeed",0,-1.2,1},
 		{"useCount",1,13,0},
 	},
 	customizationStringNames = {},
